5ffdea2e2ec617a0062825e06087e4a2d156e928
Michael Hudson <mnkotagu@gmail.com>
2023-06-01 10:53:36 +0300

    Fix 9-binary_tree_height.c bugs

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,22 +8,31 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	else
+	{
+		if (tree->left != NULL)
+		{
+			left_height = 1 + binary_tree_height(tree->left);
+		}
+		if (tree->right != NULL)
+		{
+			right_height = 1 + binary_tree_height(tree->right);
+		}
+	}
 
 	if (left_height > right_height)
 	{
-		return (1 + left_height);
+		return (left_height);
 	}
 	else
 	{
-		return (1 + right_height);
+		return (right_height);
 	}
-
 }
