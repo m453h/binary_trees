c7dbd7ac7a46f701a47d5106325baae7bca497ad
Michael Hudson <mnkotagu@gmail.com>
2023-06-01 18:44:27 +0300

    Improve 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,7 +18,11 @@ int measure_binary_tree_height(const binary_tree_t *tree)
 	l = tree->left ? 1 + measure_binary_tree_height(tree->left) : 1;
 	r = tree->right ? 1 + measure_binary_tree_height(tree->right) : 1;
 
-	return (l > r) ? l : r;
+	if (l > r)
+		return (l);
+	else
+		return (r);
+
 }
 
 /**
